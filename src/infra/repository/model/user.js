0b051a3e71fff61af5c4d4334b31e1e01bebd120
Paulo-Lopes-Estevao <pl1745240@gmail.com>
2021-12-12 23:41:48 +0100

    feat(model/user): add entity User

---
+++ src/infra/repository/model/user.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+const database = require('../database');
+
+const User = database.define('user', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    nome: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    password: { type: Sequelize.STRING }
+})
+
+module.exports = User;
